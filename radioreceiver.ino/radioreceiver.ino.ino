d2864c3eef87f11a8a83d2dbf9263d8aa1c5026d
thomed <thomasedwalker@gmail.com>
2018-05-09 15:10:48 -0600

    Single servo control

--- radioreceiver.ino/radioreceiver.ino.ino
+++ radioreceiver.ino/radioreceiver.ino.ino
@@ -2,19 +2,27 @@
 
 // receiver
 #include <RF24.h>
+#include <Servo.h>
 
 // pretty sure these are unnecessary to include
 //#include <SPI.h>
 //#include <nRF24L01.h>
 
 RF24 radio(7, 8); // CE, CSN
+Servo rightServo;
 const byte address[6] = "00001";
+
+int lastThrottle;
+int lastLeftServo;
+int lastRightServo;
+
+const int servoDeadzone = 1;
 
 // the data that needs to be received by the plane
 typedef struct {
-  int throttle;
-  int leftServo;
-  int rightServo;
+  int throttle = 0;
+  int leftServo = 90;
+  int rightServo = 90;
 } flightData;
 
 flightData data;
@@ -22,25 +30,46 @@ flightData data;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+
+  // init right servo at 90deg
+  rightServo.attach(4);
+  rightServo.write(90);
+  
   radio.begin();
   radio.openReadingPipe(0, address);
-  radio.setPALevel(RF24_PA_MIN);
+  radio.setPALevel(RF24_PA_HIGH);
   radio.startListening();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   if(radio.available()) {
-//    char text[32] = "";
-//    radio.read(&text, sizeof(text));
-//    Serial.println(text);
-    radio.read(&data, sizeof(data));
-    Serial.print("Throttle: ");
-    Serial.println(data.throttle);
-    Serial.print("Joy X: ");
-    Serial.println(data.leftServo);
-    Serial.print("Joy Y: ");
-    Serial.println(data.rightServo);
-    Serial.println();
+      // receive flight data from transmitter
+      radio.read(&data, sizeof(data));
+
+    //if(data.rightServo > rightServo.read() + servoDeadZone || data.rightServo < rightServo.read() - servoDeadZone) {
+    if(data.rightServo >= 45 && data.rightServo <= 135) {
+
+      // incrementing/decrementing by one at a time reduces chance of jitter
+        if(rightServo.read() < data.rightServo) {
+          rightServo.write(rightServo.read() + 1);
+        } else if(rightServo.read() > data.rightServo) {
+          rightServo.write(rightServo.read() - 1);
+        }  
+      
+      
+      //Serial.println(data.rightServo);
+      delay(3);
+    } 
+
+//      Serial.println("Received this:");
+//      Serial.print("Throttle: ");
+//      Serial.println(data.throttle);
+//      Serial.print("Joy X: ");
+//      Serial.println(data.rightServo);
+//      Serial.print("Joy Y: ");
+//      Serial.println(data.leftServo);
+//      Serial.println();  
   }
+
 }
--- radiotransmitter.ino/radiotransmitter.ino.ino
+++ radiotransmitter.ino/radiotransmitter.ino.ino
@@ -13,7 +13,9 @@ const byte address[6] = "00001"; // address needs to be the same for sending/rec
 // orient pins to the left
 // set a middle value on setup? ( or average between max/min? )
 int joyY = A0; // middle ~= 494, top = 1023, bottom = 0
+int joyYInit = 494;
 int joyX = A1; // middle ~= 510, far left = 1023, right = 0
+int joyXInit = 510;
 
 // logarithmic
 // header end = 0, opposite = 1023
@@ -33,14 +35,24 @@ flightData data;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-
-  // init throttle potentiometer
-  pinMode(throttlePin, INPUT);
-
+  
   // init joy
   pinMode(joyX, INPUT);
   pinMode(joyY, INPUT);
- 
+  
+  // change initial x value if abnormal
+  if(analogRead(joyX) > joyXInit + 2 || analogRead(joyX) < joyXInit - 2) {
+    joyXInit = analogRead(joyX);
+  }
+
+  // change initial y value if abnormal
+  if(analogRead(joyY) > joyYInit + 2 || analogRead(joyY) - joyYInit - 2) {
+    joyYInit = analogRead(joyY);
+  }
+
+  // init throttle potentiometer
+  // need to do max/min to calibrate ESC???
+  pinMode(throttlePin, INPUT);
 
   // init radio for transmitting
   radio.begin();
@@ -54,21 +66,34 @@ void loop() {
 
   // need to math this stuff and get real, workable values
   data.throttle = analogRead(throttlePin);
-  data.leftServo = analogRead(joyX);
-  data.rightServo = analogRead(joyY);
 
-//  Serial.print("send data size: ");
-//  Serial.println(sizeof(data));
-  Serial.print("Throttle = ");
-  Serial.println(data.throttle);
-  Serial.print("Joy X = ");
-  Serial.println(data.leftServo);
-  Serial.print("Joy Y = ");
-  Serial.println(data.rightServo);
-  Serial.println();
+//  data.leftServo = analogRead(joyX);
+//  data.rightServo = analogRead(joyY);
 
+//  Serial.println("Sending this:");
+//  Serial.print("Throttle = ");
+//  Serial.println(data.throttle);
+//  Serial.print("Joy X = ");
+//  Serial.println(data.leftServo);
+//  Serial.print("Joy Y = ");
+//  Serial.println(data.rightServo);
+//  Serial.println();
+
+  // update servo values based on joystick position before sending
+  updateJoys();
+  
   radio.write(&data, sizeof(data));
-  //const char text[] = "Hello World";
-  //radio.write(&text, sizeof(text));
-  delay(200);
 }
+
+void updateJoys() {
+  int joyXValue = analogRead(joyX);
+  int joyYValue = analogRead(joyY);
+  
+  // find what amount  between -45 and 45 degrees to move right servo
+  double rightDegDiff = (((double) joyXValue / joyXInit) * 45) - 45;
+  data.rightServo = 90 + (int)rightDegDiff;
+
+  //Serial.println(data.rightServo);
+}
+
+
